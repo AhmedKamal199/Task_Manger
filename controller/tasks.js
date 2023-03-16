f9081be6ce2b4f7af1259d2c782eaed16d8c3993
AhmedKamal199 <akm.kamal123456@gmail.com>
2023-03-16 15:12:06 +0200

    delete route

--- controller/tasks.js
+++ controller/tasks.js
@@ -30,15 +30,15 @@ const updateTask = async(req,res)=>{
 		res.status(500).json({msg:err})
 	}
 }
-const deleteTask = async(req,res)=>{
-	try{
+const deleteTask = asyncWrapper(async(req,res)=>{
 		const { id: taskID } = req.params;
 		const task = await Task.findOneAndDelete({_id: taskID});
-		res.status(201).json({task});
-	}catch(err){
-		res.status(500).json({msg:err})
-	}
+		if(!task){
+			return res.status(404).json({ msg: `No task with ${taskID}`})
+		}
+		res.status(201).json({ task });
 }
+)
 module.exports = {
     getAllTasks,
     createTask,
