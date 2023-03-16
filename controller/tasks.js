0caa9871bb23f459f7e2a8be6c6e043e09c0c320
AhmedKamal199 <akm.kamal123456@gmail.com>
2023-03-16 21:59:29 +0200

    all routes tasks

--- controller/tasks.js
+++ controller/tasks.js
@@ -3,7 +3,7 @@ const asyncWrapper = require('../middleware/async');
 
 const getAllTasks = asyncWrapper(async(req,res)=>{
 		const tasks = await Task.find({})
-		res.status(200).json({tasks})
+		res.status(200).json({ status: "Success", data: { tasks, nbHits: tasks.length}} )
 }
 )
 const createTask =asyncWrapper(async(req,res)=>{
