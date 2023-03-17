3964999c5b874c52323430e905bd645defdd71e4
AhmedKamal199 <akm.kamal123456@gmail.com>
2023-03-17 10:37:44 +0200

    Add new Custom ApI Error

---
+++ errors/custorm-error.js
@@ -0,0 +1,17 @@
+
+
+class customError extends Error{
+	constructor(message, statusCode){
+		super(message)
+		this.statusCode = statusCode
+	}
+}
+
+const createNewError = (msg, statusCode)=>{
+	return new customError(msg, statusCode)
+}
+
+module.exports = {
+	customError,
+	createNewError
+}
