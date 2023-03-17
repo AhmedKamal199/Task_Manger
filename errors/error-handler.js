3c84617751fcf0634e3058b84200b4af4c2db440
AhmedKamal199 <akm.kamal123456@gmail.com>
2023-03-17 11:45:37 +0200

    Add new Custom ApI Error

--- errors/error-handler.js
+++ errors/error-handler.js
@@ -2,10 +2,10 @@ const { customError } = require('./custorm-error')
 
 const errorHandler = (err, req, res, next)=>{
 	if (err instanceof customError ){
-		return res.status(err.statusCode),{msg: err.message}
+		return res.status(err.statusCode).json({msg: err.message}
 	}
 	return res.status(500).json({msg: "Some thing wrong try again"})
-}
+})
 
 
 module.exports = errorHandler;
